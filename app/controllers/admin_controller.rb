bc0bbfcfcb830f4df4b43bda9b9682454a2eb57e
sven <sven199109@gmail.com>
2016-10-29 21:21:51 +0800

    access control

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,10 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
+  load_and_authorize_resource
   rescue_from CanCan::AccessDenied do |exception|
-    render json: { code: 403 } ,status: 403
+    respond_to do |format|
+      format.html { render file: '/public/422.html', status: 403, layout: false }
+      format.json { render json: { code: 403 } ,status: 403 }
+    end
   end
 end
